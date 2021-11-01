f64faa26855aa1dece57adfc59ca8b9b0daef1e7
Nezertiam <mawyn.nhek@gmail.com>
2021-11-01 09:41:35 +0100

    [FIX] ProtectedRoutes

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,12 +1,15 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import { Route, Redirect } from 'react-router-dom'
 
 const ProtectedRoute = (props) => {
 
+    const roles = useSelector((state) => state.user.roles);
+
     return (
         <>
             {
-                props.isLogged ? (
+                (roles.includes(props.role) || roles.includes("ROLE_ADMIN")) ? (
                     <Route {...props} />
                 ) : (
                     <Redirect to="/" />
